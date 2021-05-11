6a3b7de084b3fdea25cd3c16530b44d431ab0bbb
unknown <jlarson129@outlook.com>
2021-05-11 12:54:10 -0400

    completed task 3

--- index.js
+++ index.js
@@ -85,7 +85,11 @@ function Person(name, age) {
   Car.prototype.fill = function(gallons) {
     this.tank += gallons;
   };
+  Car.prototype.drive = function(distance) {
+
+  }
   
+
   
   /*
     TASK 3
@@ -94,10 +98,14 @@ function Person(name, age) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
   }
- 
+  Baby.prototype = Object.create(Person.prototype)
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
+  }
   
   /* 
     TASK 4
